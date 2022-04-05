f35c92c1352fd69f6b9ffe036985adf3154e816a
Covenant <3324608778@qq.com>
2022-04-05 11:37:32 +0800

    初始化项目

---
+++ src/test/java/com/major/knowledgePlanet/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.major.knowledgePlanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
