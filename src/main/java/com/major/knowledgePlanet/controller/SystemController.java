8f6bb6672b25819d009c46e6759721e2ae80e1d4
Covenant <3324608778@qq.com>
2022-04-07 10:31:14 +0800

    修改复杂if-else结构

--- src/main/java/com/major/knowledgePlanet/controller/SystemController.java
+++ src/main/java/com/major/knowledgePlanet/controller/SystemController.java
@@ -19,6 +19,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * TODO:系统基本使用模块
  * 包含登录、注册等基本功能
+ *
  * @author 孟繁霖
  * @date 2022/4/5 23:18
  */
@@ -36,41 +37,38 @@ public class SystemController {
     private UserInfoService userInfoService;
 
     @GetMapping("system/getVerificationCode/{email}")
-    public Response getVerificationCode(@PathVariable("email") String email){
+    public Response getVerificationCode(@PathVariable("email") String email) {
         boolean isMatch = ReUtil.isMatch("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z0-9]{2,6}$", email);
-        if(!isMatch){
+        if (!isMatch) {
             Response.clientError().code("A0103").message("邮箱格式错误");
         }
-        String verificationCode= EmailUtil.getVerificationCode();
-        redisTemplate.opsForValue().set(email,verificationCode,60*10, TimeUnit.SECONDS);
+        String verificationCode = EmailUtil.getVerificationCode();
+        redisTemplate.opsForValue().set(email, verificationCode, 60 * 10, TimeUnit.SECONDS);
         //调用hutool工具类
-        MailUtil.send(email, "激活验证码", EmailUtil.getContents(email,verificationCode), true);
+        MailUtil.send(email, "激活验证码", EmailUtil.getContents(email, verificationCode), true);
         //缓存验证码,2分钟
         //TODO 验证码不返回
-        return Response.success().data("verificationCode",verificationCode);
+        return Response.success().data("verificationCode", verificationCode);
     }
 
     @PostMapping("system/register")
-    public Response register(@RequestParam(value = "nickName")String nickName,@RequestParam("email")String email,
-                             @RequestParam("verificationCode")String verificationCode,@RequestParam("password")String password){
+    public Response register(@RequestParam(value = "nickName") String nickName, @RequestParam("email") String email,
+                             @RequestParam("verificationCode") String verificationCode, @RequestParam("password") String password) {
         User user = userInfoService.getUserByEmail(email);
-        if(user!=null) {
+        if (user != null) {
             return Response.clientError().code("A0101").message("该邮箱已注册");
-        }else{
-            User newUser = new User(null, nickName, new Date(), new Date(), 1, defaultAvatar,email,password);
-            if(verificationCode.equals((String)redisTemplate.opsForValue().get(email))){
-                if(userInfoService.addUser(newUser)>0){
-                    return Response.success().data("user",newUser);
-                }else{
-                    return Response.serverError().message("新用户注册失败");
-                }
-            }
-            else{
-                return Response.clientError().code("A0104").message("邮件校验码匹配失败");
-            }
         }
+        User newUser = new User(null, nickName, new Date(), new Date(), 1, defaultAvatar, email, password);
+        if (verificationCode.equals((String) redisTemplate.opsForValue().get(email))) {
+            if (userInfoService.addUser(newUser) > 0) {
+                return Response.success().data("user", newUser).message("注册成功");
+            }
+            return Response.serverError().message("新用户注册失败");
+        }
+        return Response.clientError().code("A0104").message("邮件校验码匹配失败");
+
     }
-
-
-
 }
+
+
+
